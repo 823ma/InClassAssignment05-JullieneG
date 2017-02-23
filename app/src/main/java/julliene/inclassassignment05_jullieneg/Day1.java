05a9962f460aeb7d29ef3da07ac55a150d03d46a
jamjam11 <jmg2307@tc.columbia.edu>
2017-02-23 03:18:26 -0500

    activity day1 xml and java code update

--- app/src/main/java/julliene/inclassassignment05_jullieneg/Day1.java
+++ app/src/main/java/julliene/inclassassignment05_jullieneg/Day1.java
@@ -5,6 +5,7 @@ import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class Day1 extends AppCompatActivity {
 
@@ -16,11 +17,33 @@ public class Day1 extends AppCompatActivity {
     }
 
     public void shareNow(View view) {
+        EditText itemsList = (EditText) findViewById(R.id.list_item);
+        String items = itemsList.getText().toString();
+
+        EditText places = (EditText) findViewById(R.id.places_to_go);
+        String toDo = places.getText().toString();
+
+        String tripMessage = createTripPlan(items,toDo);
+
         Intent intent = new Intent(Intent.ACTION_SENDTO);
         intent.setData(Uri.parse("mailto:"));
         intent.putExtra(Intent.EXTRA_SUBJECT, "Come join the trip!");
+        intent.putExtra(Intent.EXTRA_TEXT, tripMessage);
         if (intent.resolveActivity(getPackageManager()) != null) {
             startActivity(intent);
+
         }
+    }
+
+    public String createTripPlan(String items, String toDo){
+
+        EditText list = (EditText) findViewById(R.id.list_item);
+        EditText toDO = (EditText) findViewById(R.id.places_to_go);
+        String tripMessage = "Location: Eiffel Tower" +
+                "\nItems to Bring: " + list +
+                "\nPlaces to Go: " + toDO +
+                "\nHope to see you soon!";
+        return tripMessage;
+
     }
 }
