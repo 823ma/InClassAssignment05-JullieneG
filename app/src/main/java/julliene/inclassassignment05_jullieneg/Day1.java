e50eb9c7e261ed786d83776c2df8e62c79a68d1b
jamjam11 <jmg2307@tc.columbia.edu>
2017-02-23 02:21:36 -0500

    main xml and style update

--- app/src/main/java/julliene/inclassassignment05_jullieneg/Day1.java
+++ app/src/main/java/julliene/inclassassignment05_jullieneg/Day1.java
@@ -1,7 +1,10 @@
 package julliene.inclassassignment05_jullieneg;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Day1 extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class Day1 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_day1);
         getSupportActionBar().setTitle("Day 1");
+    }
+
+    public void shareNow(View view) {
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:"));
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Come join the trip!");
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
     }
 }
